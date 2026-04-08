3cc574668d1f119b959fd975b095e2ff2e9ed9bf
l0iV <opezdallysyj@gmail.com>
2026-04-08 19:11:33 +0300

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,12 +7,24 @@ export default function Footer() {
     textDecoration: "none",
     color: "black",
   };
+  const BorderColor = {
+    borderLeft: "5px solid orange",
+    borderTop: "5px solid blue",
+    borderRight: "5px solid red",
+    borderBottom: "5px solid green",
+    borderRadius: "40px",
+  };
   return (
-    <section className="min-h-[100px] w-full items-center bg-[#eff5f9] flex flex-col gap-[20px] text-[20px] p-[20px] footer">
-      <h1>МБДОУ " Детский сад комбинированного вида №18"</h1>
+    <section
+      className="min-h-[100px] w-full items-center bg-[#eff5f9] flex flex-col gap-[20px] text-[20px] p-[20px] footer"
+      style={BorderColor}
+    >
+      <h1 className="text-[30px]">
+        МБДОУ " Детский сад комбинированного вида №18"
+      </h1>
       <div className="flex justify-around w-full items-center content-footer">
         <div className="flex flex-col items-center gap-[20px] w-[33%] social">
-          <h3>Мы в соц. сетях:</h3>
+          <h3 className="text-[25px">Мы в соц. сетях:</h3>
           <ul className="flex items-center justify-center gap-[10px]">
             <li>
               <img src={вк} alt="вк" className=" w-[60px] cursor-pointer" />
@@ -30,41 +42,43 @@ export default function Footer() {
           </ul>
         </div>
         <div className="flex flex-col items-center w-[33%] ">
-          <ul className="flex flex-col items-start gap-[10px] w-[33%] navigation">
+          <ul className="flex flex-col items-center gap-[10px] w-[33%] navigation">
             <h3>Навигация по сайту:</h3>
             <li>
               <NavLink to="мероприятия" style={navLinkStyle}>
-                Мероприятия
+                <p className="text-pink">Мероприятия</p>
               </NavLink>
             </li>
             <li>
               <NavLink to="родителям" style={navLinkStyle}>
-                Родителям
+                <p className="text-blue-600">Родителям</p>
               </NavLink>
             </li>
             <li>
               <NavLink style={navLinkStyle} to="оНас">
-                О нас
+                <p className="text-red-700">О нас</p>
               </NavLink>
             </li>
           </ul>
         </div>
         <ul className="flex flex-col items-start gap-[10px] w-[33%] contacty">
           <li>
-            <p>Контактный телефон</p>
-            <p>8 (48753) 2-31-92</p>
+            <p>Контактный телефон:</p>
+            <p className="text-pink">8 (48753) 2-31-92</p>
           </li>
           <li>
-            <p>Электронная почта</p>
-            <p>aleksin.ds18@tularegion.org</p>
+            <p>Электронная почта:</p>
+            <p className="text-blue-600">aleksin.ds18@tularegion.org</p>
           </li>
           <li>
-            <p>Адрес</p>
-            <p>301364, Тульская область, г. Алексин, ул. Заводская, д. 5-а</p>
+            <p>Адрес:</p>
+            <p className="text-red-700">
+              301364, Тульская область, г. Алексин, ул. Заводская, д. 5-а
+            </p>
           </li>
         </ul>
       </div>
-      <h1>Copyright © 2025. All Rights Reserved | by l0iV</h1>
+      <h1>Copyright © 2026. All Rights Reserved | by l0iV</h1>
     </section>
   );
 }
